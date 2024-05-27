88c4321a3227491db140a9d7a08bb78280fe39c9
gel <gxvl.kr@gmail.com>
2024-05-27 01:11:34 -0300

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import {
+  ClerkProvider,
+  SignedIn,
+} from '@clerk/nextjs'
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "ai-notes",
+  description: "AI-powered notes",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <html lang='en'>
+        <body>
+          <SignedIn>
+          </SignedIn>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}
